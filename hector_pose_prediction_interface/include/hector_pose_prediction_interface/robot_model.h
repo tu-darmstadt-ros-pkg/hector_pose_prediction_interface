ac3bb9000690bb6fd6555d56de30df2d2791bd59
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2021-10-14 10:35:34 +0200

    Moved pose and robot model to hector_math.

--- hector_pose_prediction_interface/include/hector_pose_prediction_interface/robot_model.h
+++ hector_pose_prediction_interface/include/hector_pose_prediction_interface/robot_model.h
@@ -5,49 +5,13 @@
 #ifndef HECTOR_POSE_PREDICTION_INTERFACE_ROBOT_MODEL_H
 #define HECTOR_POSE_PREDICTION_INTERFACE_ROBOT_MODEL_H
 
-#include "hector_pose_prediction_interface/types.h"
-#include <memory>
-#include <unordered_map>
+#include <hector_math/robot/robot_model.h>
 
 namespace hector_pose_prediction_interface
 {
 
-template<typename Scalar>
-class RobotModel
-{
-public:
-  using Ptr = std::shared_ptr<RobotModel<Scalar> >;
-  using ConstPtr = std::shared_ptr<const RobotModel<Scalar> >;
+using hector_math::RobotModel;
 
-  //! Updates the joint positions of the robot model with the given values for the corresponding joint name.
-  void updateJointPositions( const std::unordered_map<std::string, Scalar> &positions )
-  {
-    doUpdateJointPositions( positions );
-  }
-
-  //! Updates the joint positions of the robot model with the given values.
-  //! Indices have to correspond to names in getJointNames().
-  void updateJointPositions( const std::vector<Scalar> &positions ) { doUpdateJointPositions( positions ); }
-
-  //! The names of the joints represented in this robot model.
-  std::vector<std::string> getJointNames() const { return doGetJointNames(); }
-
-  std::vector<Scalar> getJointPositions() const { return doGetJointPositions(); }
-
-  //! The position of the center of mass in the robot coordinate frame.
-  math::Vector3<Scalar> centerOfMass() const { return computeCenterOfMass(); }
-
-private:
-  virtual void doUpdateJointPositions( const std::unordered_map<std::string, Scalar> &joint_positions ) = 0;
-
-  virtual void doUpdateJointPositions( const std::vector<Scalar> &positions ) = 0;
-
-  virtual std::vector<std::string> doGetJointNames() const = 0;
-
-  virtual std::vector<Scalar> doGetJointPositions() const = 0;
-
-  virtual math::Vector3<Scalar> computeCenterOfMass() const = 0;
-};
 }  // namespace hector_pose_prediction_interface
 
 #endif  // HECTOR_POSE_PREDICTION_INTERFACE_ROBOT_MODEL_H
